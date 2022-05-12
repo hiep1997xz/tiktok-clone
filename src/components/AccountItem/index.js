1cefb6bbc0c031f5ced632ed320cdb5cd82ce80f
hiep1997xz <hiep1997xz@gmail.com>
2022-05-12 10:31:12 +0700

    btn/upload-logi

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -10,7 +10,7 @@ function AccountItem() {
     <div className={cx('wrapper')}>
       <img
         className={cx('avatar')}
-        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/39aa72128edab0ecf7e78059c46b80c1~c5_100x100.jpeg?x-expires=1652284800&x-signature=eBxbUTfkEcoPDNMMK9P%2FWTa9vMQ%3D"
+        src="https://aphoto.vn/wp-content/uploads/2019/02/anh-sang-trong-nhiep-anh.jpg"
         alt="Hoaa"
       />
       <div className={cx('info')}>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -5,6 +5,7 @@ import styles from './Header.module.scss';
 import Tippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
@@ -59,7 +60,10 @@ const Header = () => {
             </button>
           </div>
         </Tippy>
-        <div className={cx('actions')}></div>
+        <div className={cx('actions')}>
+          <Button text>Upload</Button>
+          <Button primary>Log in</Button>
+        </div>
       </div>
     </header>
   );
